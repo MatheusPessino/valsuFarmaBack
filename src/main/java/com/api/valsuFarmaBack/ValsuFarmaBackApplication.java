865b725866058995c500b290b2ea4a4f42ee04b6
Matheus Pessino <paulomatheuspessino@gmail.com>
2022-10-26 10:47:07 -0300

    initial commit

---
+++ src/main/java/com/api/valsuFarmaBack/ValsuFarmaBackApplication.java
@@ -0,0 +1,14 @@
+package com.api.valsuFarmaBack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ValsuFarmaBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ValsuFarmaBackApplication.class, args);
+		System.out.println("Api ta rodando!!");
+	}
+
+}
---
+++ src/test/java/com/api/valsuFarmaBack/ValsuFarmaBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.valsuFarmaBack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ValsuFarmaBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
